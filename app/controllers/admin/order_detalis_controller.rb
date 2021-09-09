5888ccd6ca9cba6af7176e98a98501da0cce8825
EC2 Default User <ec2-user@ip-172-31-0-229.ec2.internal>
2021-09-09 10:25:14 +0000

    [Climax]ステータスの更新とか

--- app/controllers/admin/order_detalis_controller.rb
+++ app/controllers/admin/order_detalis_controller.rb
@@ -3,6 +3,23 @@ class Admin::OrderDetalisController < ApplicationController
   def update
     @order_detali = OrderDetali.find(params[:id])
     @order_detali.update(order_detali_params)
+    @order = @order_detali.order
+    if @order_detali.making_status == "making"
+
+      @order.making!
+
+    end
+    count = 0
+    @order_detalis = @order.order_detalis
+    @order_detalis.each do |order_detali|
+     if order_detali.making_status == "production_completed"
+       count += 1
+       if count == @order_detalis.count
+       @order.preparing_ship!
+       end
+     end
+   end
+
     redirect_to admin_order_path(@order_detali.order.id)
   end
 
